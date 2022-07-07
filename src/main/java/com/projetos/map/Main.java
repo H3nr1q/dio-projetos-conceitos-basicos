40e232e64c4573ec02216f250a566135fea773ee
Casa <carlos_henrique.taqua@hotmail.com>
2022-07-06 23:18:50 -0300

    Implementando o map

---
+++ src/main/java/com/projetos/map/Main.java
@@ -0,0 +1,46 @@
+package com.projetos.map;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Map<String, String> aluno = new HashMap<>();
+
+        aluno.put("Nome", "João");
+        aluno.put("Idade", "17");
+        aluno.put("Media", "8.5");
+        aluno.put("Turma", "3A");
+
+        System.out.println(aluno);
+
+        System.out.println(aluno.keySet());
+
+        System.out.println(aluno.values());
+
+        List<Map<String, String>> listAlunos = new ArrayList();
+        listAlunos.add(aluno);
+
+
+        Map<String, String> aluno2 = new HashMap<>();
+
+        aluno2.put("Nome", "Henrique");
+        aluno2.put("Idade", "33");
+        aluno2.put("Media", "9");
+        aluno2.put("Turma", "3A");
+
+        listAlunos.add(aluno2);
+
+        System.out.println(listAlunos);
+
+        System.out.println(aluno.containsKey("Nome"));
+
+        System.out.println(aluno.containsValue("Henrique"));
+        System.out.println(aluno2.containsValue("Henrique"));
+
+
+    }
+}
