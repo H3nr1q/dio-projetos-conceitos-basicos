4bbac4a749220639d4ee76c62ad7eec77dedd23e
Casa <carlos_henrique.taqua@hotmail.com>
2022-07-06 22:47:23 -0300

    Implementando estrutura de fila com queue

---
+++ src/main/java/com/projetos/queue/Main.java
@@ -0,0 +1,40 @@
+package com.projetos.queue;
+
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+    public static void main(String[] args) {
+        Queue<Carro> queueCarros = new LinkedList<>();
+
+        queueCarros.add(new Carro("Ford"));
+        queueCarros.add(new Carro("Chevrolet"));
+        queueCarros.add(new Carro("Fiat"));
+        queueCarros.add(new Carro("Peugeot"));
+
+        System.out.println(queueCarros);
+
+        System.out.println(queueCarros.add(new Carro("Renault")));
+
+        System.out.println(queueCarros);
+
+        System.out.println(queueCarros.offer(new Carro("Ferrari")));
+
+        System.out.println(queueCarros);
+
+        System.out.println(queueCarros.peek());
+
+        System.out.println(queueCarros);
+
+        System.out.println(queueCarros.poll());
+
+        System.out.println(queueCarros);
+
+        System.out.println(queueCarros.isEmpty());
+
+        System.out.println(queueCarros.size());
+
+    }
+}
+
